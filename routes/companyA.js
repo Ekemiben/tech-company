400d703373fd6fe75e95a0e6c7c3502e2f94e229
Ekemini <ekemiben.4@gmail.com>
2022-10-24 14:42:34 +0100

    last editing

--- routes/companyA.js
+++ routes/companyA.js
@@ -4,22 +4,21 @@ const router = express.Router();
 const registrationController = require("../controllers/registerController")
 
 
-// router.route('/')
-//     .get( registrationController.getAllUsers)
-
-// router.route('/registration')
-//     .post(registrationController.creatNewUser )
-
-// router.route('/userbyid/:id')
-//     .get(registrationController.getUserById)
-
-// router.route('/updatebyid/:id')
-//     .patch( registrationController.updateUser)
-
-// router.route('/deletebyid/:id')
-//     .delete( registrationController.deleteUser)
-
 router.get("/getall", registrationController.getAllUsers)
+
+router.route('/registration')
+    .post(registrationController.creatNewUser )
+
+router.route('/userbyid/:id')
+    .get(registrationController.getUserById)
+
+router.route('/updatebyid/:id')
+    .patch( registrationController.updateUser)
+
+router.route('/deletebyid/:id')
+    .delete( registrationController.deleteUser)
+
+
 
 
 module.exports = router;
